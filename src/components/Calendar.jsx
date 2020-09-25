041a5069e3ccfb59d6666e29956b41342a66ab98
saeed.mr@gmail.com <saeed.mr@gmail.com>
2020-09-25 13:04:41 +0330

    new design

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Calendar, Radio, Col, Row, Select, Spin } from 'antd';
+import { Calendar, Radio, Col, Row, Select, Spin, Timeline } from 'antd';
 import 'antd/dist/antd.css'
 import "antd/lib/calendar/style/index.less";
 import moment from 'moment';
@@ -27,18 +27,18 @@ const SCalendar = () => {
     const handleSelect = (e) => {
         console.log('e in select', format(e._d, 'yyyy-MM-dd'));
         initFetch('discover', { date: format(e._d, 'yyyy-MM-dd') });
-        getImageColor();
+        // getImageColor();
     }
 
-    const getImageColor = () => {
-        Vibrant.from(require('../assets/ax.jpeg')).getPalette()
-            .then((palette) => console.log(palette.DarkMuted._rgb, palette.DarkVibrant._rgb, palette.LightMuted._rgb, palette.LightVibrant._rgb, palette.Muted._rgb, palette.Vibrant._rgb))
+    // const getImageColor = () => {
+    //     Vibrant.from(require('../assets/ax.jpeg')).getPalette()
+    //         .then((palette) => console.log(palette.DarkMuted._rgb, palette.DarkVibrant._rgb, palette.LightMuted._rgb, palette.LightVibrant._rgb, palette.Muted._rgb, palette.Vibrant._rgb))
 
-    }
+    // }
 
     const initFetch = (type, input) => {
         const endPointTodayAiring = `${API_URL}tv/airing_today?api_key=${API_KEY}&language=en-US`;
-        setState({ ...state, loading: true });
+        setState({ loading: true });
         switch (type) {
             case 'discover':
                 fetchItems(`${API_URL}discover/tv?api_key=${API_KEY}&language=en-US&air_date.gte=${input.date}&air_date.lte=${input.date}`);
@@ -56,13 +56,12 @@ const SCalendar = () => {
             console.log(result);
             console.log(result.data.results.filter(item => item.origin_country.indexOf('US') !== -1));
             setState({
-                media: [...state.media, ...result.data.results.filter(item => item.origin_country.indexOf('US') !== -1)],
+                media: [...result.data.results.filter(item => item.origin_country.indexOf('US') !== -1)],
                 loading: false,
                 type: endpoint.indexOf("tv") === 29 ? "series" : "movie"
             });
 
             state.media.length !== 0 && console.log("state", state);
-
         });
     };
 
@@ -132,10 +131,14 @@ const SCalendar = () => {
         );
     }
     return (
-        <div>
-            <Spin spinning={state.loading}>
-                <Calendar fullscreen={true} onSelect={e => handleSelect(e)} headerRender={e => handleHeaderRender(e)} />
-                <div className="saeed" style={{ marginTop: '20px', height: '100px', width: '500px' }}>salam
+        <div className="calendar-root-container">
+            <Spin spinning={state.loading} wrapperClassName="spin-controller" >
+                <div className="calendar-container">
+                    <div className="calendar-container-1">
+                        <Calendar fullscreen={true} onSelect={e => handleSelect(e)} headerRender={e => handleHeaderRender(e)} />
+                    </div>
+                </div>
+                <div className="saeed" style={{ marginTop: '200px', width: '500px' }}>salam
                 {/* <img crossOrigin={"anonymous"}
                         ref={imgRef} src={require("../assets/ax.jpeg")} onLoad={() => {
                             const colorThief = new ColorThief();
@@ -144,15 +147,24 @@ const SCalendar = () => {
                             const result1 = colorThief.getPalette(img, 25);
                             console.log(result1);
                         }} /> */}
-                    {state.media.map(tt => (
+                    {state.media && state.media.map(tt => (
                         <div key={tt.id}>
                             <div>{tt.name}</div>
                             <img src={`${IMAGE_BASE_URL}${POSTER_SIZE}${tt.poster_path}`} />
                             {/* <img src={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${tt.backdrop_path}`} /> */}
                         </div>
 
-                    ))}</div>
+                    ))}
+
+                    <Timeline>
+                        <Timeline.Item>Create a services site 2015-09-01</Timeline.Item>
+                        <Timeline.Item>Solve initial network problems 2015-09-01</Timeline.Item>
+                        <Timeline.Item>Technical testing 2015-09-01</Timeline.Item>
+                        <Timeline.Item>Network problems being solved 2015-09-01</Timeline.Item>
+                    </Timeline>
+                </div>
             </Spin>
+            <div className="calendar-container-footer"></div>
         </div>
     );
 }
